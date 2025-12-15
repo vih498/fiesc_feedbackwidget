18ed1e6a4af8e97db80ba4259bf63994631073d6
Víh Albuquerque <ti.vih498@outlook.com>
2025-12-14 22:27:20 -0300

    Add files via upload

---
+++ lang/en/local_fiesc.php
@@ -0,0 +1,11 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'FIESC Feedback Widget';
+
+$string['feedbacktitle'] = 'Avalie o ambiente';
+$string['submit'] = 'Enviar';
+
+$string['managefeedback'] = 'Gerenciar feedbacks';
+
+$string['eventfeedbacksubmitted'] = 'Feedback enviado';
